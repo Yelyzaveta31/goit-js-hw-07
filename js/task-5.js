e957e651c9b29b8dfa4c83c304c3064355c7caf5
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-03-29 14:06:19 +0200

    task-6

--- js/task-5.js
+++ js/task-5.js
@@ -1,8 +1,4 @@
 
-// Напиши скрипт, який змінює колір фону елемента <body>
-//  через інлайн-стиль по кліку на button.change-color
-//  і задає це значення кольору текстовим вмістом для span.color.
-
 const btn = document.querySelector(".change-color");
 const span = document.querySelector(".color");
 
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,8 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const inpute = document.querySelector("input");
+const buttonCreate = document.querySelector("[data-create]");
+const buttonDestroy = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");  
